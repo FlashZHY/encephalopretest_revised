58fdaf8196db9e461830e7335154bedc4e319be7
FlashZHY <67984926+FlashZHY@users.noreply.github.com>
2020-07-09 21:39:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,21 @@
 import pandas as pd
 import numpy as np
 import math
+import warnings
+
+warnings.filterwarnings('ignore')
 
 
 def cleanse_data(df):
     # Your task here is to remove data from any ticker that isn't XXY, sort chronologically and return a dataframe
     # whose only column is 'Adj Close'
+
+    # select the stock
+    df = df.loc[df.Ticker == "XXY"]
+    # change the type of date for sorting
+    df["Date"] = pd.to_datetime(df["Date"])
+    df = df.sort_values(by="Date")
+    df = df[["Adj Close"]]
     dfclean = df
     return dfclean
 
@@ -39,16 +49,30 @@ def mc_sim(sims, days, df):
 
     # FILL OUT THE REST OF THE CODE. The above code has given you 'sims' of simulations run 'days' days into the future.
     # Your task is to return the expected price on the last day +/- the 95% confidence interval.
-    return
+
+    # Select the last row of the simulations, namely the 1000 different outcome of the last day
+    lastday_outcome = simulation_df.iloc[-1]
+    # Compute the mean and std of the outcome
+    mean = lastday_outcome.mean()
+    std = lastday_outcome.std()
+    # Use confidence interval formula to compute the upper and lower limit
+    upper_limit = mean + 1.96 * std / math.sqrt(sims)
+    lower_limit = mean - 1.96 * std / math.sqrt(sims)
+    Half_interval = 1.96 * std / math.sqrt(sims)
+    CI = [lower_limit, upper_limit]
+
+    return mean, Half_interval, CI
+
 
 def main():
     filename = '20192020histdata.csv'
     rawdata = pd.read_csv(filename)
     cleansed = cleanse_data(rawdata)
-    simnum = 1  # change this number to one that you deem appropriate
+    simnum = 3000  # change this number to one that you deem appropriate
     days = 25
     mc_sim(simnum, days, cleansed)
-    return
+    print("Expected price, half of the interval and 95% confidence interval are:", '\n', mc_sim(simnum, days, cleansed))
+    # return
 
 
 if __name__ == '__main__':
